db020b5c2d3a84e2ab46ca01f951b091531e5e3b
Aparna <aparnamolks.off@gmail.com>
2024-07-07 03:45:46 +0530

    Update build script

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,7 @@
+#!/bin/bash
+
+# Install dependencies
 pip install -r requirements.txt
-python manage.py collectstatic
+
+# Collect static files (if you're using Django, for example)
+python manage.py collectstatic --noinput
