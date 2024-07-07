2c4526537b40529bcf512fe9692976da28690007
Aparna <aparnamolks.off@gmail.com>
2024-07-07 20:16:15 +0530

    Update build.sh file

--- build_files.sh
+++ build_files.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
+
 # Exit immediately if a command exits with a non-zero status
 set -e
 
-# Install dependencies
-pip install -r requirements.txt
+# Use the Python and pip installed by Vercel
+python3 -m pip install -r requirements.txt
 
 # Run database migrations
-python manage.py migrate
+python3 manage.py migrate
 
 # Collect static files
-python manage.py collectstatic --noinput
+python3 manage.py collectstatic --noinput
